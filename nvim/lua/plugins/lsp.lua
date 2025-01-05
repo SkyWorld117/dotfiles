4adec7f7c81d101b1da3b888d4897688f90e3338
SkyWorld117 <alanzhu02@gmail.com>
2025-01-05 23:24:48 +0100

    More setups for other languages

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -25,7 +25,13 @@ return {
         "neovim/nvim-lspconfig",
         config = function()
             local lspconfig = require("lspconfig")
+            lspconfig.bashls.setup({})
+            lspconfig.ast_grep.setup({})
+            lspconfig.fortls.setup({})
+            lspconfig.pylsp.setup({})
             lspconfig.lua_ls.setup({})
+            lspconfig.yamlls.setup({})
+            lspconfig.julials.setup({})
         end
     }
 }
