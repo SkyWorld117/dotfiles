37c58f96044ee3855b0e3e111b3502be1a6d3696
SkyWorld117 <alanzhu02@gmail.com>
2025-03-10 11:27:33 +0100

    Use yabai as default instead of aerospace

---
+++ .config/sketchybar/sketchybar/bar.lua
@@ -0,0 +1,10 @@
+local colors = require("colors")
+
+-- Equivalent to the --bar domain
+sbar.bar({
+  topmost = "window",
+  height = 40,
+  color = colors.transparent,
+  padding_right = 8,
+  padding_left = 8,
+})
