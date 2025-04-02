6114fae8dc1d6c14f0886a1b96b0618f18bd9312
SkyWorld117 <alanzhu02@gmail.com>
2025-04-02 17:42:36 +0200

    No need to filter out Orion Preview, focus the same window after moving it to another workspace

--- .config/skhd/quit_if_last.sh
+++
@@ -1,9 +0,0 @@
-#!/opt/homebrew/bin/bash
-
-window_pid=$(yabai -m query --windows --window | jq -r '.pid') 
-count_pid=$(yabai -m query --windows | jq "[.[] | select(.pid == ${window_pid})] | length")
-if [ "$count_pid" -gt 1 ]; then
-	yabai -m window --close
-else
-    kill "${window_pid}"
-fi
