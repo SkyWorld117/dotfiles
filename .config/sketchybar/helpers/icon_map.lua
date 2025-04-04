63cde35537e1404d0330d8ce3288e0857e9ede42
SkyWorld117 <alanzhu02@gmail.com>
2025-04-04 11:05:02 +0200

    Update app icons

--- .config/sketchybar/helpers/icon_map.lua
+++ .config/sketchybar/helpers/icon_map.lua
@@ -342,7 +342,7 @@ return {
 	["Yuque"] = ":yuque:",
 	["语雀"] = ":yuque:",
 	["Zed"] = ":zed:",
-	["Zen"] = ":zen_browser:",
+	["Zen Browser"] = ":zen_browser:",
 	["Zeplin"] = ":zeplin:",
 	["zoom.us"] = ":zoom:",
 	["Zotero"] = ":zotero:",
