2484193d6444de15beb010cc2eb3af9c187c30cc
SkyWorld117 <alanzhu02@gmail.com>
2025-04-07 12:53:38 +0200

    Filter out unicode prefix garbage in WhatsApp

--- .config/skhd/quit_on_last.sh
+++ .config/skhd/quit_on_last.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 window_pid=$(yabai -m query --windows --window | jq -r '.pid') 
-app_name=$(yabai -m query --windows --window | jq -r '.app')
+app_name=$(yabai -m query --windows --window | jq -r '.app' | LC_ALL=C sed 's/[^[:print:]]//g')
 count_pid=$(yabai -m query --windows | jq "[.[] | select(.pid == ${window_pid})] | length")
+
 if [ "$count_pid" -gt 1 ]; then
 	yabai -m window --close
 else
