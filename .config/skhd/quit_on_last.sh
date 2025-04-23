ff1d03b8f213331b03e709f01a8c570b7e58b0c4
SkyWorld117 <alanzhu02@gmail.com>
2025-04-23 19:27:13 +0200

    Don't quit finder

--- .config/skhd/quit_on_last.sh
+++ .config/skhd/quit_on_last.sh
@@ -4,7 +4,7 @@ window_pid=$(yabai -m query --windows --window | jq -r '.pid')
 app_name=$(yabai -m query --windows --window | jq -r '.app' | LC_ALL=C sed 's/[^[:print:]]//g')
 count_pid=$(yabai -m query --windows | jq "[.[] | select(.pid == ${window_pid})] | length")
 
-if [ "$count_pid" -gt 1 ]; then
+if [ "$count_pid" -gt 1 ] || [ "$app_name" = "Finder" ]; then
 	yabai -m window --close
 else
     osascript -e "tell application \"${app_name}\" to quit"
