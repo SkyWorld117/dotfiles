e15685ec94236ae415d6ef8a03d647779e92d038
SkyWorld117 <alanzhu02@gmail.com>
2025-09-19 14:46:43 +0200

    Update lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,38 +1,36 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    "bashls",
-                    "ast_grep",
-                    "fortls",
-                    "pylsp",
-                    "lua_ls",
-                    "yamlls",
-                    "julials"
-                },
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.bashls.setup({})
-            lspconfig.ast_grep.setup({})
-            lspconfig.fortls.setup({})
-            lspconfig.pylsp.setup({})
-            lspconfig.lua_ls.setup({})
-            lspconfig.yamlls.setup({})
-            lspconfig.julials.setup({})
-        end
-    }
+	{
+		"mason-org/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"bashls",
+					"ast_grep",
+					"fortls",
+					"pylsp",
+					"lua_ls",
+					"yamlls",
+					"julials",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			vim.lsp.config("bashls", {})
+			vim.lsp.config("ast_grep", {})
+			vim.lsp.config("fortls", {})
+			vim.lsp.config("pylsp", {})
+			vim.lsp.config("lua_ls", {})
+			vim.lsp.config("yamlls", {})
+			vim.lsp.config("julials", {})
+		end,
+	},
 }
-
